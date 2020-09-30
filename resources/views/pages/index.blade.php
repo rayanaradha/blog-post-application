37e56b90c0f3228f964bf3829a666d7c17f50af1
rayanaradha <rayanaradha1993@gmail.com>
2020-10-01 05:05:13 +0530

    change the text on home page

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,8 +3,8 @@
 
 @section('content')
 <div class="jumbotron text-center">
-  <h1> {{$title}}</h1>
-  <p>Hi, Welcome all to my laravel demo project</p>
+  <h1> Welcome to My Blog!! </h1>
+  <p>Hi, Welcome to the Blog Site. Share your knowledge with others</p>
   <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a> <a class="btn btn-success btn-lg" href="/register" role="button">Register</a></p>
 </div>
 @endsection       
