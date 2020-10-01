5d036ae85cd4ce4d35480c507deb19945cf7d9df
Rayan Aradha <46860744+rayanaradha@users.noreply.github.com>
2020-10-01 12:00:04 +0530

    Update about.blade.php

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -4,12 +4,10 @@
         <h1> About  </h1>
         <p>This is a blog , is an online journal or informational website displaying information in the reverse chronological order,
              with the latest posts appearing first, at the top. It is a platform where a writer or a group of writers share their views on an individual subject.</p>
-             
-            <p> Blogs started out as a natural extension of the increased use of computers and the creation of the earliest forms of the Internet as government military, scientific and academic networks. Before the World Wide Web, communities of people interacted on these networks. Individuals created content for themselves or others and stored that content on computers that were connected to the networks. Before blogs became popular, these communities often communicated and shared frequently-updated content through community message boards. The term “newsgroups” became coined to describe many of these discussion and information-sharing areas.
 
-The earliest true blogs started appearing around 1994 or 1995 as open access diaries where individuals shared updates about their lives, such as personal thoughts and facts about events related to their families, academic studies, career, travels and other topics. Early online diary writers include Claudio Pinhanez, Justin Hall and Carolyn Burke. The media and public really started noticing the content and documenting its formation around approximately 1996 and 1997.
-
-The exact date and year of the first use of the term “blog” is still debated by scholars. Most believe that it happened in 1999 as the natural truncation of a particular description of this type of content, “web log” or “weblog,” into the shortened form. Some debate exists on pronunciation. Obviously, the term describes a diary or log located and accessible on the World Wide Web. As time passed, some people thought that “weblog” should be pronounced “we blog” to refer to individuals performing the action of blogging.
-</p>
-
+        <p> The content of a blog typically includes text, pictures, videos, animated GIFs and even scans from old physical offline diaries or journals and other hard copy documents. Since a blog can exist 
+         merely for personal use, sharing information with an exclusive group or to engage the public, a blog owner can set their blog for private or public access.
+        Blog content can appear as posts on one continuous streaming page or posts on individual pages reachable through one or more pages set up in a list-style format as post title links, excerpts and related tags. 
+        All posts or links to posts are typically displayed to readers in reverse chronological order with the most recent content appearing first.
+        </p>
 @endsection      
