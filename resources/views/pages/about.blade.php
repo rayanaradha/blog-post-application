bfdc44bf06d89e50027d759c09f631d83e607efa
rayanaradha <rayanaradha1993@gmail.com>
2020-10-01 10:42:57 +0530

    Fill the about section

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -2,5 +2,6 @@
 
 @section('content')
         <h1> About  </h1>
-        <p>This is about page</p>
+        <p>This is a blog , is an online journal or informational website displaying information in the reverse chronological order,
+             with the latest posts appearing first, at the top. It is a platform where a writer or a group of writers share their views on an individual subject.</p>
 @endsection      
