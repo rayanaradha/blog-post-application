49186c197249c682092b7fccfd2b5e17e5b48735
rayanaradha <rayanaradha1993@gmail.com>
2020-10-01 10:45:10 +0530

    Fill the detail about service section

--- resources/views/pages/service.blade.php
+++ resources/views/pages/service.blade.php
@@ -2,5 +2,6 @@
 
 @section('content')
         <h1> Services  </h1>
-        <p>This is service page</p>
+        <p>A blog site that lets you create and maintain a blog. It hosts your blog, which means it gives your blog its own web address, or URL. Your 
+            blog service also stores your blog's data. Each blog service has different features, but they all let you share your opinions, interests, photos, and more.</p>
 @endsection      
